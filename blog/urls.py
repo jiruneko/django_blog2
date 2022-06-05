5d167ed9e01e06b3f415532b9b07ef6d33a17d6d
jiruneko <oohikaru310@gmail.com>
2022-06-05 15:54:49 +0900

    first commit

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+# from importlib.resources import path
+from django.urls import path
+from . import views
+
+app_name = 'blog'
+
+urlpatterns = [
+  # path('', views.index_view, name='index'),
+  path('', views.IndexView.as_view(), name='index'),
+]
